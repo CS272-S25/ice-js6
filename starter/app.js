4a1f88a100215ddaf82d154e8605729d2392aeba
Cole Nelson <ctnelson2@wisc.edu>
2025-03-05 11:57:26 -0600

    ice-js6

---
+++ starter/app.js
@@ -0,0 +1,45 @@
+console.log("Hello world!");
+
+// These are global variables! I am giving you permission to use them :)
+// Any function in this file can modify it.
+let story = [];
+let actions = [];
+
+// As an extension of this exercise, can you add a redo button?
+// HINT: You'll want to add another list!
+
+/**
+ * Called whenever the "Add to story!" button is clicked.
+ * Should prompt the user and add their string to the story.
+ */
+function addToStory() {
+    alert("I should add a string to story!")
+}
+
+/**
+ * Called whenever the "Delete from story!" button is clicked.
+ * Should confirm with the user to delete the last string of the story.
+ */
+function deleteFromStory() {
+    alert("I should remove the last string from story!")
+}
+
+/**
+ * Called whenever the "Undo!" button is clicked.
+ * Should undo the last action.
+ */
+function undo() {
+    alert("I should undo the last action!")
+}
+
+/**
+ * Should be called any time you want to update the story-text
+ * element to reflect the current story.
+ * HINT: You can use innerHTML to insert HTML tags!
+ *       There are security concerns with this, but we'll
+ *       discuss those in the future :)
+ */
+function updateStory() {
+    const storyNode = document.getElementById("story-text");
+    // TODO Update the text in story node...
+}
